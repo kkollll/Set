f578bc5c3e43b658d44481362bdc9a6e3960224d
Wr2020 <comin_008@163.com>
2020-06-12 13:58:21 +0800

    804 morse code

---
+++ src/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class Solution {
+
+    public int uniqueMorseRepresentations(String[] words) {
+
+        String[] codes = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+        HashSet<String> set = new HashSet<>();
+        for (String word : words) {
+
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                res.append(codes[word.charAt(i) - 'a']);
+            }
+            set.add(res.toString());
+        }
+        return set.size();
+    }
+}
